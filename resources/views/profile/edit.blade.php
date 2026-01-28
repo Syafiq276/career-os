9859d83296729a57684ca199a658c11ea2bbfde2
Muhammad Syafiq Norhazwan Bin Nor Ramzi <fathazwan14@gmail.com>
2026-01-28 20:11:08 +0800

    Fix profile page content not displaying - add slot support to app layout

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,32 +5,34 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <!-- GitHub Integration -->
-            <div class="p-4 sm:p-8 bg-gradient-to-r from-slate-800 to-slate-700 shadow sm:rounded-lg border-2 border-emerald-500">
-                <div class="max-w-xl">
-                    @include('profile.partials.github-integration')
+    <x-slot name="content">
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <!-- GitHub Integration -->
+                <div class="p-4 sm:p-8 bg-gradient-to-r from-slate-800 to-slate-700 shadow sm:rounded-lg border-2 border-emerald-500">
+                    <div class="max-w-xl">
+                        @include('profile.partials.github-integration')
+                    </div>
                 </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.update-profile-information-form')
+                    </div>
                 </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.update-password-form')
+                    </div>
                 </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.delete-user-form')
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </x-slot>
 </x-app-layout>
